ed2a2a430c32d1a86c3690503da4b06ce8b39830
SGInnovacion <37309493+SGInnovacion@users.noreply.github.com>
2021-01-25 11:38:21 +0100

    Update constants.js

--- ayto-dashboard/src/constants.js
+++ ayto-dashboard/src/constants.js
@@ -1,2 +1,17 @@
+  
+/*
+* Licencia con arreglo a la EUPL, Versión 1.2 o –en cuanto sean aprobadas por la
+Comisión Europea– versiones posteriores de la EUPL (la «Licencia»);
+* Solo podrá usarse esta obra si se respeta la Licencia.
+* Puede obtenerse una copia de la Licencia en:
+* http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+* Salvo cuando lo exija la legislación aplicable o se acuerde por escrito, el programa
+distribuido con arreglo a la Licencia se distribuye «TAL CUAL», SIN GARANTÍAS NI
+CONDICIONES DE NINGÚN TIPO, ni expresas ni implícitas.
+* Véase la Licencia en el idioma concreto que rige los permisos y limitaciones que
+establece la Licencia.
+*/
+
+
 export const DIALOG_URL = 'https://dialogflow.cloud.google.com/#/agent/97884977-7ed7-4631-aa4b-a4cc98c0f5b1/analytics';
 export const ALEXA_URL = 'https://www.amazon.com/ap/signin?openid.pape.preferred_auth_policies=Singlefactor&clientContext=133-8757922-1866725&openid.pape.max_auth_age=7200&openid.return_to=https%3A%2F%2Fdeveloper.amazon.com%2Falexa%2Fconsole%2Fask%2Fmeasure%2Famzn1.ask.skill.dd0bfccd-a624-4907-b356-ff7c61fbbb49%2Fdevelopment%2Fes_ES%2Fsummary&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_dante_us&openid.mode=checkid_setup&marketPlaceId=ATVPDKIKX0DER&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&';
